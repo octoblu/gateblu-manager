2ad795e07d9590671685f969036d6939b1966c17
Sqrt of Saturn <sqrt@octoblu.com>
2016-02-29 14:10:21 -0700

    v1.4.0 list available connectors

--- src/containers/configure-gateblu.js
+++ src/containers/configure-gateblu.js
@@ -2,6 +2,7 @@ import _ from 'lodash'
 
 import React, { Component, PropTypes } from 'react'
 import {getDevice} from '../services/devices-service'
+import {getAvailableConnectors} from '../services/connectors-service'
 
 import Loading from '../components/loading'
 import ErrorMsg from '../components/error'
@@ -10,29 +11,41 @@ export default class ConfigureGateblu extends Component {
   state = {
     loading: true,
     gateblu: null,
-    error: null
+    gatebluError: null,
+    connectorError: null
   }
 
   componentDidMount() {
     this.setState({ loading: true })
     const {uuid} = this.props.params
-    getDevice(uuid, (error, gateblu) => {
-      console.log('response', error, gateblu)
-      this.setState({error, gateblu, loading: false})
+    getDevice(uuid, (gatebluError, gateblu) => {
+      this.setState({gatebluError, gateblu, loading: false})
+    })
+    getAvailableConnectors((connectorError, connectors)=>{
+      this.setState({connectorError, connectors})
     })
   }
 
   render() {
-    const { loading, gateblu, error } = this.state
+    const { loading, gateblu, gatebluError, connectorError, connectors } = this.state
 
     if (loading) return <Loading message="Loading Gateblu..."/>
-    if (error) return <ErrorMsg errorMessage={error} />
+    if (gatebluError) return <ErrorMsg errorMessage={gatebluError.message} />
+    if (connectorError) return <ErrorMsg errorMessage={connectorError.message} />
     if (_.isEmpty(gateblu)) return <h3>Missing Gateblu</h3>
 
     const {name} = gateblu
 
+    let connectorItems = _.map(connectors, (connector) => {
+      return <li>{connector.name} ({connector.type})</li>
+    })
+
     return <div>
       <h2>{name} : Gateblu</h2>
+      <h3>Available Connectors</h3>
+      <ul>
+        {connectorItems}
+      </ul>
     </div>
   }
 }
---
+++ src/services/connectors-service.js
@@ -0,0 +1,17 @@
+import _ from 'lodash'
+import request from 'superagent'
+import {getMeshbluConfig} from './auth-service'
+
+const OCTOBLU_URL='https://api.octoblu.com'
+
+export function getAvailableConnectors(callback){
+  const {uuid, token} = getMeshbluConfig()
+  request
+    .get(`${OCTOBLU_URL}/api/node_types`)
+    .auth(uuid, token)
+    .end((error, response) => {
+      if(!response.ok) return callback(new Error('Unable to fetch connectors'))
+      if(_.isEmpty(response.body)) return callback()
+      callback(null, _.filter(response.body, {category:'device'}))
+    })
+}
