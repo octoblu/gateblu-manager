d6825379700c69748db1d81c8cd2ae188181649a
Sqrt of Saturn <sqrt@octoblu.com>
2016-03-02 13:03:50 -0700

    Add Top Bar

--- src/containers/layout.js
+++ src/containers/layout.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { TopBar, TopBarTitle } from 'zooid-ui'
 
 import Authenticated from '../containers/authenticated.js'
+
+import 'zooid-ui/dist/style.css'
 
 export default class Layout extends Component {
   render() {
     return <Authenticated>
-      <h1>Gateblu Manager</h1>
+      <TopBar>
+        <TopBarTitle>Gateblu Manager</TopBarTitle>
+      </TopBar>
       {this.props.children}
     </Authenticated>
   }
